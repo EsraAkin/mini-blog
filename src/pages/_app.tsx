9e65006097197caaf8063e264c7e2253048941ee
Esra Akineden <e.akineden@gmail.com>
2025-07-09 23:39:46 +0200

    traffic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css'; 
+import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Script from 'next/script'; // 👈 Script eklendi
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* 👇 Plausible Analytics kodu */}
+      <Script
+        defer
+        data-domain="mini-blog-cejr.vercel.app"
+        src="https://plausible.io/js/script.js"
+        strategy="afterInteractive"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
-
